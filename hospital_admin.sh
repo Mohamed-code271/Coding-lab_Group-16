73b9003dda01e165665c5f180762474fa46ff257
Mohamed-code271 <a.mohamed1@alustudent.com>
2026-06-03 23:47:26 +0200

    Update hospital_admin.sh

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -21,24 +21,3 @@ initialize_system() {
 }
 
 initialize_system
-
-
-# securing code (-2-)
-
-#!/bin/bash
-
-File="active_logs"
-
-PERM=$(stat -c "%a" "$File")
-
-ls -l "$File"
-
-if [ "$PERM" -eq 700 ]
-then
-    echo "$File is protected"
-else
-    chmod 700 "$File"
-    echo "$File is under protection"
-fi
-
-ls -l "$File"
