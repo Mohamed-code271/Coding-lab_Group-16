bef53c676b7bfcd3898875fad417f162523a0670
Luxe-Gwiz <l.gayawira@alustudent.com>
2026-06-03 15:39:00 -0700

    Arrange the .sh

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -17,7 +17,7 @@ initialize_system() {
         mkdir reports
     fi
 
-    echo "System directories are ready!" - $(date)
+    echo "System directories are ready! - $(date)"
 }
 
 initialize_system
@@ -25,13 +25,11 @@ initialize_system
 
 # Member 2 - The Security Lead
 
-
-
 #This function secures the active_logs directory
 
 #Only the owner will have read and write permissions
 
-Secure_data() {
+secure_data() {
           chmod 600 active_logs
            ls -l active_logs
            echo “permissions secured  for active_logs”
