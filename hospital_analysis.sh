9345773902e67ea54b35911a8ebbb2e63cfd8603
aubin <c.karaha@alustudent.com>
2026-06-04 11:47:35 +0300

    Added comments to water_audit function

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -17,6 +17,7 @@ process_vitals() {
 process_vitals
 
 # Member 6 - The Facility Auditor
+# This function calculates average water usage for ICU
 water_audit() {
     echo "Running water audit..."
 
