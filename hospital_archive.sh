032440c83cb37638e5842199294ae115a8a4f1d5
ynshuti-cloud <y.nshuti@alustudent.com>
2026-06-05 04:20:27 -0700

    added archive_logs function

---
+++ hospital_archive.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Member 4 - The Archivist
+# This script rotates the logs
+
+archive_logs() {
+    # Get current timestamp
+    TIMESTAMP=$(date +"%Y%m%d_%H%M")
+
+    # Move heart rate log
+    mv active_logs/heart_rate_log.log archived_logs/heart_rate_${TIMESTAMP}.log
+
+    # Move temperature log
+    mv active_logs/temperature_log.log archived_logs/temperature_${TIMESTAMP}.log
+
+    # Move water usage log
+    mv active_logs/water_usage_log.log archived_logs/water_usage_${TIMESTAMP}.log
+
+    echo "Logs archived with timestamp: $TIMESTAMP"
+
+    # Recreate empty log files so engine keeps running
+    touch active_logs/heart_rate_log.log
+    touch active_logs/temperature_log.log
+    touch active_logs/water_usage_log.log
+
+    echo "Empty log files recreated in active_logs"
+}
+
+# Call the function
+archive_logs
